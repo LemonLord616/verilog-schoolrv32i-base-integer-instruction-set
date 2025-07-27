5d40c696ecb0429d6bc26e7347a70ca8a158b62b
Marat M. <m.mestnikov@innopolis.university>
2025-07-27 21:31:35 +0300

    add r-type constants in header file, adjust ALU constant size, add instruction-listing-done table (track what is done)

--- rtl/sr_cpu.svh
+++ rtl/sr_cpu.svh
@@ -14,23 +14,38 @@
 
 // ALU commands
 
-`define ALU_ADD     3'b000
-`define ALU_OR      3'b001
-`define ALU_SRL     3'b010
-`define ALU_SLTU    3'b011
-`define ALU_SUB     3'b100
+`define ALU_ADD     4'b0000
+`define ALU_OR      4'b0001
+`define ALU_SRL     4'b0010
+`define ALU_SLTU    4'b0011
+`define ALU_SUB     4'b0100
+// New ones TODO: implement
+`define ALU_SLL     4'b0101
+`define ALU_SLT     4'b0110
+`define ALU_XOR     4'b0111
+`define ALU_SRA     4'b1000
+`define ALU_AND     4'b1001
 
 // Instruction opcode
 
-`define RVOP_ADDI   7'b0010011
-`define RVOP_BEQ    7'b1100011
-`define RVOP_LUI    7'b0110111
-`define RVOP_BNE    7'b1100011
+`define RVOP_ADDI   7'b0010011 // I-type
+`define RVOP_BEQ    7'b1100011 // B-type
+`define RVOP_LUI    7'b0110111 // U-type
+`define RVOP_BNE    7'b1100011 // B-type
+
+// R-type
 `define RVOP_ADD    7'b0110011
 `define RVOP_OR     7'b0110011
 `define RVOP_SRL    7'b0110011
 `define RVOP_SLTU   7'b0110011
 `define RVOP_SUB    7'b0110011
+// New ones (R-type) TODO: implement
+`define RVOP_SLL    7'b0110011
+`define RVOP_SLT    7'b0110011
+`define RVOP_XOR    7'b0110011
+`define RVOP_SRA    7'b0110011
+`define RVOP_AND    7'b0110011
+
 `define RVOP_ANY    7'b???????
 
 // Instruction funct3
@@ -44,6 +59,12 @@
 `define RVF3_SLTU   3'b011
 `define RVF3_SUB    3'b000
 `define RVF3_ANY    3'b???
+// New ones (R-type) TODO: implement
+`define RVF3_SLL    3'b001
+`define RVF3_SLT    3'b010
+`define RVF3_XOR    3'b100
+`define RVF3_SRA    3'b101
+`define RVF3_AND    3'b111
 
 // Instruction funct7
 
@@ -52,6 +73,13 @@
 `define RVF7_SRL    7'b0000000
 `define RVF7_SLTU   7'b0000000
 `define RVF7_SUB    7'b0100000
+// New ones (R-type) TODO: implement
+`define RVF7_SLL    7'b0000000
+`define RVF7_SLT    7'b0000000
+`define RVF7_XOR    7'b0000000
+`define RVF7_SRA    7'b0100000
+`define RVF7_AND    7'b0000000
+
 `define RVF7_ANY    7'b???????
 
 `endif  // `ifndef SR_CPU_SVH
